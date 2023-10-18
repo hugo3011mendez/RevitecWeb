46bec5f348daf03f02d43b2ffe3cb9723695a697
Hugo <hugo3011mendez@gmail.com>
2023-10-18 19:40:23 +0200

    Eliminado todo lo no necesario, me quedo con Home y el Navbar

--- React/RevitecWeb/src/components/Navbar.js
+++ React/RevitecWeb/src/components/Navbar.js
@@ -1,26 +1,10 @@
 import React, { useState, useContext } from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import smTournamentLogo from '../resources/tournamentLogo.png';
-import lgTournamentLogo from '../resources/tournamentLogo.png';
-import smOrganizationLogo from '../resources/SpainCroquetLandLogo.png';
-import lgOrganizationLogo from '../resources/SpainCroquetLandLogo.png';
 import BsButton from '../collection/bootstrap/BsButton'
 import './Navbar.scss';
 
 export default function NavigationBar(props) {
-
-    // Login
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    /***** show login *****/
-    const showLoginModal = () => {
-        setIsModalOpen(true);
-    }
-    /***** close login *****/
-    const closeLoginModal = () => {
-        setIsModalOpen(false);
-    }
 
     return (
         <Navbar collapseOnSelect expand={'xxxl'} className="bg-main" id="navbar-expand-xxxl" variant="dark" sticky="top">
@@ -28,10 +12,7 @@ export default function NavigationBar(props) {
                 <LinkContainer to="/">
                     <Navbar.Brand>
                         <div className="d-sm-inline-block">
-                            <img src={smTournamentLogo} className="logo d-sm-none" alt="Tournament Logo" />
-                            <img src={lgTournamentLogo} className="logo d-sm-inline-block d-none" alt="Tournament Logo" />
-                            <img src={smOrganizationLogo} className="logo d-sm-none" alt="Organization Logo" />
-                            <img src={lgOrganizationLogo} className="logo d-sm-inline-block d-none" alt="Organization Logo" />
+                            {/*<img src={smTournamentLogo} className="logo d-sm-none" alt="Tournament Logo" />*/}
                         </div>
                         <h4 className="d-none d-sm-inline-block ms-2">Torneo Camino de Santiago</h4>
                     </Navbar.Brand>
