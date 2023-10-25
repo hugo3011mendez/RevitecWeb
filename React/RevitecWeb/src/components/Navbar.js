4eda92e5be0cb974a2bc22c8e28d90a2d1ced649
Hugo <hugo3011mendez@gmail.com>
2023-10-25 18:04:46 +0200

    - Changed favicon and web title - Added logo and a test dropdown to Navbar

--- React/RevitecWeb/src/components/Navbar.js
+++ React/RevitecWeb/src/components/Navbar.js
@@ -2,28 +2,26 @@ import React, { useState, useContext } from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import BsButton from '../collection/bootstrap/BsButton'
-import './Navbar.scss';
+import imgLogoRevitec from '../resources/logo_revitec.png'
 
 export default function NavigationBar(props) {
 
     return (
-        <Navbar collapseOnSelect expand={'xxxl'} className="bg-main" id="navbar-expand-xxxl" variant="dark" sticky="top">
+        <Navbar collapseOnSelect expand={'xxxl'} id="navbar-expand-xxxl" className="border-bottom" sticky="top">
             <Container className="container-fluid ms-5">
                 <LinkContainer to="/">
                     <Navbar.Brand>
-                        <div className="d-sm-inline-block">
-                            {/*<img src={smTournamentLogo} className="logo d-sm-none" alt="Tournament Logo" />*/}
+                        <div className="d-sm-inline-block ps-5">
+                            <img src={imgLogoRevitec} style={{ height: '3.5rem' }} alt="Revitec Logo" />
                         </div>
-                        <h4 className="d-none d-sm-inline-block ms-2">Torneo Camino de Santiago</h4>
                     </Navbar.Brand>
                 </LinkContainer>
 
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav" className="text-center bg-main">
+                <NavDropdown title="Servicios" id="basic-nav-dropdown">
                     <LinkContainer to="/view/messages">
-                        <BsButton className="btn-primary">Mensajes</BsButton>
+                        <NavDropdown.Item className="text-center">Fachadas</NavDropdown.Item>
                     </LinkContainer>
-                </Navbar.Collapse>
+                </NavDropdown>
             </Container>
         </Navbar>
     );
